a61409ae7455337695dd57b3dbe19f5b94915bc6
DarthNixilis <65317550+DarthNixilis@users.noreply.github.com>
2026-01-04 15:28:08 -0700

    Enhance card rendering with persona and starting cards
    Added new render functions for persona and starting cards.

--- js/main.js
+++ js/main.js
@@ -4,7 +4,12 @@ import { loadCards } from "./data/loadCards.js"
 import { getCardPool } from "./cardPool.js"
 import { PersonaManager } from "./personaManager.js"
 import { DeckManager } from "./deckManager.js"
-import { renderCardPool, renderDeck } from "./ui.js"
+import {
+  renderCardPool,
+  renderDeck,
+  renderPersona,
+  renderStartingCards
+} from "./ui.js"
 import { loadTSV } from "./tsvLoader.js"
 
 async function init() {
@@ -17,22 +22,28 @@ async function init() {
   const cardPool = getCardPool(allCards)
 
   renderCardPool(cardPool, onCardClicked)
-  renderDeck(deck.getCards())
+  renderDeck([])
 
   function onCardClicked(card) {
-    // Persona selection
     if (card.rules?.isPersona) {
       deck.clear()
       deck.addCard(card)
 
       const autoCards = personaManager.selectPersona(card)
-      autoCards.forEach(c => deck.addCard(c))
 
+      renderPersona(card)
+
+      const startingCards = autoCards.filter(
+        c => c.rules?.isStartingCard
+      )
+
+      startingCards.forEach(c => deck.addCard(c))
+
+      renderStartingCards(startingCards)
       renderDeck(deck.getCards())
       return
     }
 
-    // Normal add
     deck.addCard(card)
     renderDeck(deck.getCards())
   }
